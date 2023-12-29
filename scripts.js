a511a9d425a15cfc1ffd4fd91c50055e339ebf71
Robert Duncan <robertdthree@gmail.com>
2023-12-28 17:53:08 -0800

    grid working

---
+++ scripts.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const grid = document.querySelector('#grid');
+
+    // Creating a 16x16 grid to place squares in
+    for (let i = 0; i < 16; i++){
+        const row = document.createElement('div');
+        row.classList.add('row');
+        for (let j = 0; j < 16; j++){
+            const square = document.createElement('span');
+            square.classList.add('square');
+            row.appendChild(square);
+        }
+        grid.appendChild(row);
+    }
+});
